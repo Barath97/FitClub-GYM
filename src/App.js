633b0248682f20942a9b50fdb2d4dd4ffba8c462
Barath97 <barath.kj97@gmail.com>
2024-02-16 23:56:24 +0530

    Hero and Header Component created and initialized

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Hero from './component/Hero/Hero';
+function App() {
+  return (
+    <div className="App">
+          <Hero/>
+    </div>
+  );
+}
+
+export default App;
